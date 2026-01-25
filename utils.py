77bf89cb8363977d274ef953546614b6dd741fa7
Agata Benvegna <Agataben23@gmail.com>
2026-01-25 10:04:38 +0100

    Add "AverageValueMeter()" class

--- utils.py
+++ utils.py
@@ -10,3 +10,21 @@ def set_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
 
+class AverageValueMeter():
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.sum = 0
+        self.num = 0
+
+    def add(self,value,num):
+        self.sum += value*num
+        self.num += num
+
+    def value(self):
+        try:
+            return self.sum/self.num
+        except:
+            return None
+
