aaebd28483c4003b7f470b874720dcfc1f56fb7c
Agata Benvegna <Agataben23@gmail.com>
2026-01-26 19:51:27 +0100

    Use "build_toy_dataset()" in tests

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -5,6 +5,7 @@ import random
 
 from PIL import Image
 from utils import set_seed
+from utils import build_toy_dataset
 from dataset.preprocessing import download_food101
 from dataset.preprocessing import build_image_path_label_df
 from dataset.preprocessing import split_dataset
@@ -16,13 +17,8 @@ class TestPreprocessing(unittest.TestCase):
         seed = 1238
         set_seed(seed)
 
-        # Make a toy image for tests
-        # Image wants array with uint8 values from 0 to 255 for RGB image
-        # TODO: make a function in utils
-        array = (np.random.rand(384, 384, 3) * 255).astype(np.uint8)
-        img = Image.fromarray(array)
-        os.makedirs('tests/images/toy', exist_ok=True)
-        img.save('tests/images/toy/1234.jpg')
+        # Build toy dataset
+        build_toy_dataset(samplings_n = 1)
 
     def test_download_food101(self):
         pass
@@ -30,10 +26,10 @@ class TestPreprocessing(unittest.TestCase):
     def test_build_image_path_label_df(self):
         class_names, labels, data_df = build_image_path_label_df('tests', True)
         self.assertEqual(len(class_names), 1)
-        self.assertEqual(class_names[0], 'toy')
+        self.assertEqual(class_names[0], 'class_0')
         self.assertEqual(len(labels), 1)
         self.assertEqual(labels[0], 0)
-        self.assertEqual(data_df.loc[0,'path'], 'images/toy/1234.jpg')
+        self.assertEqual(data_df.loc[0,'path'], 'images/class_0/0.jpg')
         self.assertEqual(data_df.loc[0,'label'], 0)
 
     def test_split_dataset(self):
