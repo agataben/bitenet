fe8f947af81846942be4290f2f230be978f0ba5b
Agata Benvegna <Agataben23@gmail.com>
2026-01-27 22:06:22 +0100

    Change test on the modele

--- tests/test_bitenet_v1.py
+++ tests/test_bitenet_v1.py
@@ -4,6 +4,7 @@ import numpy as np
 import random
 
 from models.bitenet_v1.bitenet_v1 import BiteNetV1
+from utils import set_seed
 from PIL import Image
 
 class TestBiteNetV1(unittest.TestCase):
@@ -11,20 +12,16 @@ class TestBiteNetV1(unittest.TestCase):
     def setUp(self):
         # Set seed
         seed = 1238
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
-        np.random.seed(seed)
-        random.seed(seed)
-
+        set_seed(seed)
 
     def test_output_size_conv_layers(self):
         model = BiteNetV1()
-        x = torch.randn(1,3,384,384)
+        x = torch.randn(1,3,224,224)
         x_1 = model.conv_1(x)
         x_2 = model.conv_2(x_1)
         x_3 = model.conv_3(x_2)
 
-        self.assertEqual(x_1.shape, torch.Size([1,18,95,95]))
-        self.assertEqual(x_2.shape, torch.Size([1,28,23,23]))
-        self.assertEqual(x_3.shape, torch.Size([1,28,5,5]))
+        self.assertEqual(x_1.shape, torch.Size([1,18,55,55]))
+        self.assertEqual(x_2.shape, torch.Size([1,28,13,13]))
+        self.assertEqual(x_3.shape, torch.Size([1,28,3,3]))
 
